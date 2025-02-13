f5a734bcfe7d5794d50c5e6f0a67843c917d89b0
Raghav <dev@raghav98.com>
2025-02-13 14:16:00 +0530

    first and second pass

---
+++ karpathy-gpt-from-scratch/second-pass.py
@@ -0,0 +1,95 @@
+import torch as t
+import torch.nn as nn
+import torch.optim as optim
+import requests as r
+
+# Hyper params
+train_test_split = 0.8
+batch_size = 32
+context_size = 8
+iters = 50000
+reporting_iters = 5000
+
+# Data loading and encoding/ decoding
+dataset_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+data = r.get(dataset_url)
+data = data.text
+
+unique_chars = sorted(list(set(data)))
+i_to_c = {i: c for i, c in enumerate(unique_chars)}
+c_to_i = {c: i for i, c in enumerate(unique_chars)}
+
+def encode_string(s):
+    return [c_to_i[c] for c in s]
+
+def decode(t):
+    return "".join([i_to_c[i] for i in t])
+
+# More data loading
+data = t.LongTensor(encode_string(data))
+num_train = int(len(data) * train_test_split)
+train, test = data[:num_train], data[num_train:]
+
+def get_batch(split):
+    to_batch = train if split == "train" else test
+    idxs =  t.randint(len(to_batch) - context_size, (batch_size, ))
+    x = t.stack([to_batch[i:i+context_size] for i in idxs])
+    y = t.stack([to_batch[i+1:i+context_size+1] for i in idxs])
+    return x, y
+    
+
+# Model definition
+language_size = len(unique_chars)
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = nn.Embedding(language_size, language_size)
+
+    def forward(self, x, target=None):
+        # x (B, T), target (B, T)
+        logits = self.embedding(x) # (B, T, C)
+        if target is not None:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            target = target.view(B*T)
+            loss = nn.functional.cross_entropy(logits, target)
+        else:
+            loss = None
+        return logits, loss
+
+    def generate(self, idxs, max_num_tokens):
+        for _ in range(max_num_tokens):
+            logits, _ = self(idxs)
+            probs = nn.functional.softmax(logits[:, -1, :], dim=-1)
+            idx_next = t.multinomial(probs, num_samples=1)
+            idxs = t.concat([idxs, idx_next], dim=-1)
+        return idxs
+
+def compute_loss(model, loss):
+    model.eval()
+    x, y = get_batch('train')
+    _, loss = model.forward(x, y)
+    print(f"Loss is {loss}")
+    model.train()
+
+# Train
+model = BigramLanguageModel()
+optim = optim.Adam(model.parameters())
+
+for i in range(iters):
+    x, y = get_batch('train')
+    _, loss = model.forward(x, y)
+    if loss is None:
+        raise ValueError("loss is none")
+    optim.zero_grad(set_to_none=True)
+    loss.backward()
+    optim.step()
+    if i % reporting_iters == 0:
+        compute_loss(model, loss)
+
+# Try on a random output
+generate_tokens = 1000
+print(decode(model.generate(t.zeros((1, 1), dtype=t.long), generate_tokens)[0].tolist()))
+
+
